f5c5d2a0182db8d20bb10c5bdaa1cf0d15729588
nyfair <nyfair2012@gmail.com>
2022-04-13 23:42:31 +0800

    Update autoupdate.py

--- autoupdate.py
+++ autoupdate.py
@@ -7,7 +7,7 @@ x = json.loads(resp.read().decode('utf-8'))
 mingw = x['Mingw-w64'][:x['Mingw-w64'].find('ucrt')+4]
 with in_place.InPlace('.github/workflows/toolchain.yml', newline='') as f:
   for l in f:
-    if (i:=l.find('key: mcf_')) > 99:
+    if (i:=l.find('key: mcf_')) > -1:
       l = '%s%s\n' % (l[:i+9], mingw)
     elif (i:=l.find('curl')) > -1:
       l = '%s%s.7z\n' % (l[:i+55], x['Mingw-w64'])
@@ -34,7 +34,7 @@ pkgs['ffmpeg-dev'] = pkgs['ffmpeg']
 for t in ['batch-stable.yml', 'batch-bleeding_edge-weekly.yml']:
   with in_place.InPlace('.github/workflows/%s' % t, newline='') as f:
     for l in f:
-      if (i:=l.find('key: mcf_')) > 99:
+      if (i:=l.find('key: mcf_')) > -1:
         l = '%s%s\n' % (l[:i+9], mingw)
       elif (i:=l.find('/dev/')) > -1:
         r = l.find('-1-x86_64')
